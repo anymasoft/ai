a60c01104b85395149dc25ff8eb0614d2a2e93ff
qqqUser <User@WIN>
2026-01-27 01:35:47 +0300

    del

--- JobRadar/main.py
+++ JobRadar/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse, RedirectResponse
-from fastapi.staticfiles import StaticFiles
+# from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 async def index():
